691f462cd7b9946be35f3670c9fcdfccea727351
Ryan Delk <ryancdelk@gmail.com>
2018-07-09 22:08:39 -0500

    Removed old app.js, renamed app2.js to take its place

--- js/app.js
+++ js/app.js
@@ -1,138 +1,160 @@
 ///////////////////////////////////////////////////////////////////////////////
+//  MASTER JS
 //
-//                              Enemy stuff
+//
+//
+//  INFORMATION
+//  Project.................Classic Arcade Game
+//  Purpose.................Udacity Front-End Nanodegree Program
+//                          Project 3
+//  Creator.................Ryan Delk
+//  Last Change.............July 9, 2018
+//
+//
+//
+//  CONTENTS
+//  1. Constructors
+//  2. Instantiation
+//
+//
+//
+//  NOTES
+//    To jump to a specific section, search (CTRL + F) for the
+//    section's primary heading number, followed by its
+//    subsection's letter.
+//
+//    e.g. Search for "2b" to go to the Squares/Styling section
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-// Enemies our player must avoid
-var Enemy = function(x, y, speed) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+
+
+///////////////////////////////////////
+//  1. Constructors
+///////////////////////////////////////
+
+// Default sprite constructor
+class Sprite {
+
+  // Pass in parameters to control sprite speed and location
+  constructor(x, y, speed) {
     this.x = x;
     this.y = y;
     this.speed = speed;
+    this.sprite = '';
+  };
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+  // Draw the sprite on the canvas
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  };
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+// Sprite subclass for enemies
+class SpriteEnemy extends Sprite {
+
+  // Take same parameters as parent class, but use specific sprite
+  constructor(x, y, speed) {
+    super(x, y, speed);
+    this.sprite = 'images/enemy-bug.png';
+  };
+
+  // Control changes to the enemy when stuff happens
+  update(dt) {
     this.x += this.speed * dt;
 
-    // when off canvas, reset position of enemy to move across again
+    // Reset enemy positions and speed when they reach the edge
     if (this.x > 550) {
-        this.x = -100;
-        this.speed = 100 + Math.floor(Math.random() * 512);
+      this.x = -100;
+      this.speed = 100 + Math.floor(Math.random() * 512);
     }
 
-    // Check for collision between player and enemies
+    // Collision detection
     if (player.x < this.x + 60 &&
-        player.x + 37 > this.x &&
-        player.y < this.y + 25 &&
-        30 + player.y > this.y) {
-        player.x = 200;
-        player.y = 380;
+      player.x + 37 > this.x &&
+      player.y < this.y + 25 &&
+      30 + player.y > this.y) {
+      player.x = 200;
+      player.y = 380;
     }
+  };
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+// Sprite subclass for player
+class SpritePlayer extends Sprite {
 
-
-
-///////////////////////////////////////////////////////////////////////////////
-//
-//                              Player stuff
-//
-///////////////////////////////////////////////////////////////////////////////
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-const Player = function(x, y, speed) {
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
+  // Take same parameters as parent class, but use specific sprite
+  constructor(x, y, speed) {
+    super(x, y, speed);
     this.sprite = 'images/char-boy.png';
-};
+  };
 
-Player.prototype.update = function() {
-};
+  // Default function for updating
+  update() {};
 
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-Player.prototype.handleInput = function(allowedKeys) {
-    "use strict";
+  // Player controls
+  handleInput(allowedKeys) {
     switch (allowedKeys) {
-        case "left":
-            //check for wall, otherwise move left
-            if (this.x > 0) {
-                this.x -= 51;
-            }
-            break;
-        case "right":
-            //check for wall, otherwise move right
-            if (this.x < 402) {
-                this.x += 51;
-            }
-            break;
-        case "up":
-            //check if player reached top of water, if so call success function,
-            // otherwise move up
-            if (this.y < 0) {
-                this.success();
-            } else {
-                this.y -= 41.5;
-            }
-            break;
-        case "down":
-            //check for bottom, otherwise move down
-            if (this.y < 300) {
-                this.y += 41.5;
-            }
-            break;
+      case "left":
+        // Don't allow movement past right edge
+        if (this.x > 0) {
+          this.x -= 101;
+        }
+        break;
+      case "right":
+        // Don't allow movement past left edge
+        if (this.x < 402) {
+          this.x += 101;
+        }
+        break;
+      case "up":
+        // Don't allow movement past top edge
+        if (this.y < 0) {
+          this.success();
+        } else {
+          this.y -= 83;
+        }
+        break;
+      case "down":
+        // Don't allow movement past bottom edge
+        if (this.y < 350) {
+          this.y += 83;
+        }
+        break;
     }
+  }
 };
 
-let player = new Player(200, 380, 50);
+
+
+///////////////////////////////////////
+//  2. Instantiation
+///////////////////////////////////////
+
+// Create a new player sprite
+let player = new SpritePlayer(200, 380, 50);
+
+// Initialize empty array to hold enemies
 let allEnemies = [];
-// var enemy;
 
-// enemyPosition.forEach(function(posY) {
-//     enemy = new Enemy(0, posY, 100 + Math.floor(Math.random() * 512));
-//     allEnemies.push(enemy);
-// });
-
-// Instantiate enemies
+// Creating enemies
 for (var i = 0; i < 3; i++) {
-    //startSpeed is a random number from 1-10 times speedMultiplier
-    var startSpeed = 5 * Math.floor(Math.random() * 10 + 1);
-    //enemys start off canvas (x = -100) at the following Y positions: 60, 145, 230
-    allEnemies.push(new Enemy(-100, 60 + (85 * i), startSpeed));
-}
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-// But I modified it so that the player moves on 'keydown' instead of 'keyup'.
-document.addEventListener('keydown', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  // Set speed at which enemies will travel
+  const startSpeed = 20 * Math.floor(Math.random() * 10 + 1);
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  // Push each new enemy to the array
+  allEnemies.push(new SpriteEnemy(-100, 60 + (85 * i), startSpeed));
+};
+
+// Listen for keypress to control character
+document.addEventListener('keydown', function (e) {
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
+
+  player.handleInput(allowedKeys[e.keyCode]);
 });
